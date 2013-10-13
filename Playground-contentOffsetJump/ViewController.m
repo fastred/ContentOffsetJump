e934bd3795237adec185b5d32dba1be6f9a0b61b
Arkadiusz Holko <fastred@fastred.org>
2013-10-13 14:27:20 +0200

    Add collection view with proper scrolling based on contentInset

--- Playground-contentOffsetJump/ViewController.m
+++ Playground-contentOffsetJump/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomSpaceConstraint;
 
 @end
 
@@ -17,13 +20,56 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    self.textField.delegate = self;
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+
+    if (indexPath.row < 28) {
+        cell.backgroundColor = [UIColor yellowColor];
+    } else {
+        cell.backgroundColor = [UIColor redColor];
+    }
+
+    return cell;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 76;
+}
+
+- (void)updateKeyboardConstraint:(CGFloat)height animationDuration:(NSTimeInterval)duration {
+    self.bottomSpaceConstraint.constant = height;
+    [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+
+        [self.collectionView setContentInset:UIEdgeInsetsMake(0, 0, height, 0)];
+
+        CGPoint bottomOffset = CGPointMake(0, self.collectionView.contentSize.height - (self.collectionView.bounds.size.height - height));
+        [self.collectionView setContentOffset:bottomOffset animated:YES];
+        [self.view layoutIfNeeded];
+
+    } completion:nil];
+}
+
+- (void)keyboardWillShow:(NSNotification *)notif
+{
+    CGFloat height = [[[notif userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size.height;
+    [self updateKeyboardConstraint:height animationDuration:0.25];
+    NSLog(@"%f", height);
 }
 
 @end
